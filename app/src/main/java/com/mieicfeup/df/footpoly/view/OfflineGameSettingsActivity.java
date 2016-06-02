6544a6e6431c8f193ed17fa5c591aa8381771b34
Caramelo18 <fabiocaramelo18@gmail.com>
2016-06-02 13:04:12 +0100

    OfflineGameSettingsActivity added, allowing to select number of players

--- app/src/main/java/com/mieicfeup/df/footpoly/view/OfflineGameSettingsActivity.java
+++ app/src/main/java/com/mieicfeup/df/footpoly/view/OfflineGameSettingsActivity.java
@@ -6,17 +6,27 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 import com.mieicfeup.df.footpoly.R;
+
+import java.util.ArrayList;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class MenuActivity extends AppCompatActivity {
+public class OfflineGameSettingsActivity extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -27,13 +37,13 @@ public class MenuActivity extends AppCompatActivity {
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
      */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 500;
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
-    private static final int UI_ANIMATION_DELAY = 5;
+    private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
@@ -87,52 +97,84 @@ public class MenuActivity extends AppCompatActivity {
         }
     };
 
+    private int numPlayers;
+    private Button playButton;
+    private ListView playersList;
+    private SeekBar players;
+    private ArrayList<String> playersNames;
+    private SparseBooleanArray checkedPlayers;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_offline_game_settings);
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
 
-        // Set up the user interaction to manually show or hide the system UI.
-      /*  mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-              //  toggle();
-            }
-        });*/
-
         hide();
+        playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchGame(v);
+            }
+        });
+
+        playersList  = (ListView) findViewById(R.id.playerList);
+        playersList.setChoiceMode(playersList.CHOICE_MODE_MULTIPLE);
+
+        playersNames = new ArrayList<String>();
+        playersNames.add("Player 1");
+        playersNames.add("Player 2");
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, playersNames);
+        playersList.setAdapter(adapter);
+
+        players = (SeekBar) findViewById(R.id.numPlayers);
+
+        players.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                numPlayers = progress + 2;
+                playersNames.clear();
+                for(int i = 1; i <= numPlayers; i++)
+                {
+                    playersNames.add("Player " + String.valueOf(i));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        playersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                checkedPlayers = playersList.getCheckedItemPositions();
+                Log.w("new listener", String.valueOf(checkedPlayers.get(0)));
+            }
+        });
+
+
+
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-      //  findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-
-        Button offlineButton = (Button) findViewById(R.id.offlineButton);
-        offlineButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchOfflineSettingsActivity(v);
-            }
-        });
-
-        Button onlineButton = (Button) findViewById(R.id.onlineButton);
-
-        Button optionsButton = (Button) findViewById(R.id.optionsButton);
-
-        Button exitButton = (Button) findViewById(R.id.exitButton);
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                System.exit(0);
-            }
-        });
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
@@ -143,13 +185,6 @@ public class MenuActivity extends AppCompatActivity {
         // created, to briefly hint to the user that UI controls
         // are available.
         delayedHide(100);
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        hide();
     }
 
     private void toggle() {
@@ -195,11 +230,9 @@ public class MenuActivity extends AppCompatActivity {
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
-    public void launchOfflineSettingsActivity(View view)
+    public void launchGame(View view)
     {
-        Intent intent = new Intent(this, OfflineGameSettingsActivity.class);
+        Intent intent = new Intent(this, GameActivity.class);
         this.startActivity(intent);
     }
-
-
 }
