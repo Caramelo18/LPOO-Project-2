bd9101799c7382d1c943928627b5e3cdaa15b301
Caramelo18 <fabiocaramelo18@gmail.com>
2016-06-07 16:21:44 +0100

    Stadium prices updated

--- app/src/main/java/com/mieicfeup/df/footpoly/model/Table.java
+++ app/src/main/java/com/mieicfeup/df/footpoly/model/Table.java
@@ -22,33 +22,33 @@ public class Table implements Serializable {
         luck = new Luck(freeParking);
 
         places.put(0, new StartingPoint());
-        places.put(1, new Stadium("Emirates Stadium", "England", 500, 251));
-        places.put(2, new Stadium("White Hart Lane", "England", 500, 251));
-        places.put(3, new Stadium("Old Trafford", "England", 500, 251));
-        places.put(4, new Stadium("Anfield", "England", 500, 251));
+        places.put(1, new Stadium("Emirates Stadium", "England", 450, 300));
+        places.put(2, new Stadium("White Hart Lane", "England", 400, 250));
+        places.put(3, new Stadium("Old Trafford", "England", 500, 325));
+        places.put(4, new Stadium("Anfield", "England", 400, 250));
 
         places.put(5, jail);
 
-        places.put(6, new Stadium("Camp Nou", "Spain", 500, 251));
+        places.put(6, new Stadium("Camp Nou", "Spain", 550, 350));
         places.put(7, luck);
-        places.put(8, new Stadium("Santiago Bernabeu", "Spain", 500, 251));
-        places.put(9, new Stadium("Vicente Calderon", "Spain", 500, 251));
+        places.put(8, new Stadium("Santiago Bernabeu", "Spain", 550, 350));
+        places.put(9, new Stadium("Vicente Calderon", "Spain", 500, 325));
 
         places.put(10, freeParking);
 
-        places.put(11, new Stadium("Signal Iduna Park", "Germany", 500, 251));
-        places.put(12, new Stadium("Allianz Arena", "Germany", 500, 251));
+        places.put(11, new Stadium("Signal Iduna Park", "Germany", 650, 425));
+        places.put(12, new Stadium("Allianz Arena", "Germany", 650, 425));
 
-        places.put(13, new Stadium("Juventus Stadium", "Italy", 500, 251));
-        places.put(14, new Stadium("San Siro", "Italy", 500, 251));
+        places.put(13, new Stadium("Juventus Stadium", "Italy", 600, 400));
+        places.put(14, new Stadium("San Siro", "Italy", 600, 400));
 
         places.put(15, new GoToJail(jail));
 
         places.put(16, luck);
 
-        places.put(17, new Stadium("Estadio da Luz", "Portugal", 500, 251));
-        places.put(18, new Stadium("Estadio Jose Alvalade", "Portugal", 500, 251));
-        places.put(19, new Stadium("Estadio do Dragao", "Portugal", 500, 251));
+        places.put(17, new Stadium("Estadio da Luz", "Portugal", 750, 500));
+        places.put(18, new Stadium("Estadio Jose Alvalade", "Portugal", 700, 450));
+        places.put(19, new Stadium("Estadio do Dragao", "Portugal", 700, 450));
     }
 
     /**
